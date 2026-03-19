6ed365c90ecf35846020d3b79df71a8f18d4726f
dinith-heshan <dinithheshansp@gmail.com>
2026-03-19 18:06:39 +0530

    fix: update session.headers (remove extra encodings and user agents)

--- spider.py
+++ spider.py
@@ -63,11 +63,10 @@ class ThrottledSession:
             "User-Agent": (
                 "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
                 "(KHTML, like Gecko) Chrome/120.0 Safari/537.36 "
-                "FinlexSpider/1.0"
             ),
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Encoding": "gzip, deflate",
             "Connection": "keep-alive",
         })
 
