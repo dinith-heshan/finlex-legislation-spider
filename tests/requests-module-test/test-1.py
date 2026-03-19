e60a8b473576f6ad41e9bd0c7980dfeca4acda9e
dinith-heshan <dinithheshansp@gmail.com>
2026-03-19 11:02:34 +0530

    test: update request test

--- tests/requests-module-test/test-1.py
+++ tests/requests-module-test/test-1.py
@@ -1,4 +1,18 @@
 import requests
 
-x = requests.get('https://w3schools.com/python/demopage.html')
-x = requests.delete('https://w3schools.com/python/demopage.php')
+session = requests.Session()
+
+session.headers.update({
+    "User-Agent": (
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/120.0 Safari/537.36 "
+        "FinlexSpider/1.0"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Connection": "keep-alive",
+})
+
+r = requests.get("https://httpbin.org/get")
+print(r.text)
